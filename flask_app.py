b4f11444d66bf4bc45e59df7fc83094c3f967a27
johannsc <johann.schmidt@ovgu.de>
2020-07-19 19:33:02 +0200

    fixed collapsible

--- flask_app.py
+++ flask_app.py
@@ -9,6 +9,9 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/convolution')
+def blog_convolution():
+    return render_template('convolution.html')
 
 if __name__ == '__main__':
     app.run()
