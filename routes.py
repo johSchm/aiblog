3369e5c43ba8a8b022e6b64bbefd56398fe7c9d9
johannsc <johann.schmidt@ovgu.de>
2020-10-10 09:44:51 +0200

    fixed hyperlinks and added temp

--- routes.py
+++ routes.py
@@ -10,3 +10,11 @@ def home():
 @app.route('/convolution')
 def blog_convolution():
     return render_template('convolution.html')
+
+@app.route('/publications')
+def publications():
+    return render_template('publications.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
